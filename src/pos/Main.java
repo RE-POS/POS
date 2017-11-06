32d46d4eca9bec309ecd9d662cfe5fd8f46904e2
Myles <mylesandre1124@gmail.com>
2017-11-06 17:55:50 -0500

    fixes

--- src/pos/Main.java
+++ src/pos/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("posmain.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("posMain.fxml"));
         primaryStage.setTitle("Point Of Sale");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
